37c2ce93f561b9fc4cb136bccf5871c54923297f
silulika <held.lisa@gmail.com>
2017-06-26 14:27:50 +0200

    Create sw.js

---
+++ bluetooth/arcade/sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('arcade').then(cache => {
+      return cache.addAll(['index.html'])
+      .then(_ => self.skipWaiting());
+    })
+  )
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
